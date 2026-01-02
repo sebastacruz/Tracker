4e286e8634d995262e229f73e35dc325a0e29352
Tristan McVay <tristanmcvay@Tristans-MacBook-Air.local>
2026-01-02 13:28:36 -0800

    Update Vite config to build to docs folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Tracker/',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     port: 5173,
     open: true,
